505fa09ac3c9b3f92140dce090bdd4dd25d7c2fb
KomalGupta1137 <komalgupta1137@gmail.com>
2023-05-13 17:19:32 +0530

    server updated

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/users', (request, response) => {
 //Request for some specific users by id
 app.get('/user/:id', (request, response) => {
     const id = request.params.id
-    connection.query('SELECT * FROM Users WHERE Id=?',[request.params.id], (err, rows) => {
+    connection.query('SELECT * FROM Users WHERE Id=?',[id], (err, rows) => {
         if(err){
             console.log(err)
         }
@@ -33,13 +33,51 @@ app.get('/user/:id', (request, response) => {
 })
 
 
+//Request for deleting  row in tables
+app.delete('/user/:id',(request,response) => {
+    var id = request.params.id
+    var query = 'DELETE FROM Users WHERE Id =?'
+    connection.query(query,id,(error, rows) => {
+        if(error){
+            console.log(error)
+        }
+        else{
+            response.send(rows)
+        }
+    })
+})
 
 
+//Request for storing row in specific tables by id
+app.post('/user', (request, response) => {
+     var bodyData = request.body
+     var userData = [bodyData.Id, bodyData.Name, bodyData.Age]
+     var query = 'INSERT INTO Users(Id, Name, Age) VALUES(?)'
+     connection.query(query,[userData] ,(error, rows) => {
+        if(error){
+            console.log(error)
+        }else{
+            response.send(rows)
+            console.log(rows)
+        }
+    })
+
+})
 
 
+//Updating the rows of table by id
+app.put('/user/:id',(request, response) => {
+    var bodyData = request.body
+    var query = 'UPDATE Users SET ? WHERE Id = ' + bodyData.id
+    connection.query(query,[bodyData], (error,rows) => {
+        if (error){
+            console.log(error)
+        }else{
+            response.send(rows)
+        }
+    })
 
-
-
+})
 
 
 app.listen(3000, (req,res) => {
